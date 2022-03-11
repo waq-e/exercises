f4db704f9b31810830dd7a5208b854319b0a7f1a
waq-e <whomomlikes@hotmail.com>
2022-03-10 23:28:40 -0600

    Exercise 3.13

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
-// 3.12 (Date Class) Create a class called Date that includes three pieces of information as data
-// members—a month (type int), a day (type int) and a year (type int). Your class should have a
-// constructor with three parameters that uses the parameters to initialize the three data members.
-// For the purpose of this exercise, assume that the values provided for the year and day are correct,
-// but ensure that the month value is in the range 1–12; if it isn’t, set the month to 1. Provide a
-// set and a get function for each data member. Provide a member function displayDate that displays
-// the month, day and year separated by forward slashes (/). Write a test program that demonstrates
-// class Date’s capabilities.
+// Fig. 3.9: AccountTest.cpp
+// Displaying and updating Account balances.
 #include <iostream>
-#include "Date.h"
+#include "Account.h"
 
 using namespace std;
 
 int main()
 {
-	Date d1{12, 26, 1967};
-	d1.displayDate();
-	Date d2{17, 3, 1885};
-	d2.displayDate();
+	Account account1{"Jane Green", 50};
+	Account account2{"John Blue", -7};
 
-	return 0;
+	// display initial balance of each object
+	cout << "account1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance();
+
+	cout << "\n\nEnter deposit amount for account1: "; // prompt
+	int depositAmount;
+	cin >> depositAmount; // obtain user input
+	cout << "adding " << depositAmount << " to account1 balance";
+	account1.deposit(depositAmount); // add to account1's balance
+
+	// display balances
+	cout << "\n\naccount1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance();
+
+	cout << "\n\nEnter deposit amount for account2: "; // prompt
+	cin >> depositAmount; // obtain user input
+	cout << "adding " << depositAmount << " to account2 balance";
+	account2.deposit(depositAmount); // add to account2 balance
+
+	// display balances
+	cout << "\n\naccount1: " << account1.getName() << " balance is $"
+	     << account1.getBalance();
+	cout << "\naccount2: " << account2.getName() << " balance is $"
+	     << account2.getBalance() << endl;
 }
