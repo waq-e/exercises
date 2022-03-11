c10cbab11047489d0e610c531c2b2785e21a128d
waq-e <whomomlikes@hotmail.com>
2022-03-11 06:39:08 -0600

    Exercise 4.16

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
-// Exercise 4.15
+// Exercise 4.16
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	double hoursWorked{0.0};
-	cout << "Enter number of hours worked (-1 to quit): ";
-	cin >> hoursWorked;
+	double totalFundsRaised{0.0};
+	int lapsCompleted{0};
+	cout << "Enter laps completed (-1 to quit): ";
+	cin >> lapsCompleted;
 
-	while( hoursWorked >= 0) {
-		double accruedLeave{0.0};
-		accruedLeave += 2.0;
-		accruedLeave += hoursWorked * 0.10;
-		cout << "Accrued leave " << accruedLeave << '\n';
-		cout << "Enter number of hours worked (-1 to quit): ";
-		cin >> hoursWorked;
+	while( lapsCompleted >= 0) {
+		int bonusLapsStartAt{40};
+		double sponsorshipRate{0.0};
+		cout << "Enter sponsorship rate: ";
+		cin >> sponsorshipRate;
+		double lapsBonus{0.0};
+	double studentContribution{0.0};
+		if (lapsCompleted > bonusLapsStartAt) {
+			lapsBonus = (lapsCompleted - bonusLapsStartAt) * 1.5 * sponsorshipRate;
+			studentContribution = sponsorshipRate * static_cast<double>(bonusLapsStartAt);
+			studentContribution += lapsBonus;
+		} else {
+			studentContribution = sponsorshipRate * static_cast<double>(lapsCompleted) + lapsBonus;
+		}
+		totalFundsRaised += studentContribution;
+		cout << "Student contribution is: " << studentContribution << '\n';
+		cout << "Enter laps completed (-1 to quit): ";
+		cin >> lapsCompleted;
 	}
+	cout << "Total funds raised: " << totalFundsRaised << '\n';
 	return 0;
 }
