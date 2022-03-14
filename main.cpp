9ce3728994c131ae443f9fcbcb91fa9095778e10
waq-e <whomomlikes@hotmail.com>
2022-03-14 06:28:00 -0500

    Exercise 4.22

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-// Exercise 4.21: Mystery2.cpp
+// Exercise 4.22: Mystery3.cpp
 #include <iostream>
 using namespace std;
-int main(){
-	int count = 0;
-	while ( count <= 12 ) {
-		cout << ( count % 3 ? "--->" : "######" ) << endl;
-		++count;
+int main() {
+	int row {12};
+	unsigned int column;
+	while ( row >= 0 ) {
+		column = 1;
+		while ( column <= 6 ) {
+			if (row % 3 == 0) {
+				cout << "#";
+			}
+			else {
+				cout << ( column % 6 ? "-" : ">" ); // output
+			}
+			++column;
+		}
+		--row;
+		cout << endl;
 	}
 }
