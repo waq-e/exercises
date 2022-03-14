721dac6f635b188ef36e03fb82b0915fb06f4995
waq-e <whomomlikes@hotmail.com>
2022-03-14 06:18:16 -0500

    Exercise 4.20

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
-// Exercise 4.16
+// Fig. 4.14: Analysis.cpp
+// Analysis of examination results using nested control statements.
 #include <iostream>
-
 using namespace std;
-
 int main() {
-	int counter{1};
-	int larger{0};
-	int largest{0};
-
-	while (counter <= 10) {
-		cout << "Enter #" << counter++ << ": ";
-		int number{};
-		cin >> number;
-		if (number > larger && number > largest) {
-			larger = largest;
-			largest = number;
+// initializing variables in declarations
+	unsigned int passes{0};
+	unsigned int failures{0};
+	unsigned int studentCounter{1};
+// process 10 students using counter-controlled loop
+	while (studentCounter <= 10) {
+// prompt user for input and obtain value from user
+		cout << "Enter result (1 = pass, 2 = fail): ";
+		int result;
+		cin >> result;
+// if...else is nested in the while statement
+		if (result == 1) {
+			passes = passes + 1;
 		}
-		if (number > larger && number < largest) {
-			larger = number;
+		else {
+			failures = failures + 1;
 		}
+// increment studentCounter so loop eventually terminates
+		studentCounter = studentCounter + 1;
 	}
-	cout << "The larger number was " << larger << '\n';
-	cout << "The largest number was " << largest << '\n';
-
-	return 0;
+// termination phase; prepare and display results
+	cout << "Passed: " << passes << "\nFailed: " << failures << endl;
+// determine whether more than 8 students passed
+	if (passes > 8) {
+		cout << "Bonus to instructor!" << endl;
+	}
 }
