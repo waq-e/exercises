663d35380508a8e6bf44020cc2505c37b13bb4d6
waq-e <whomomlikes@hotmail.com>
2022-03-14 06:30:14 -0500

    Exercise 4.26

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int row {12};
-	unsigned int column;
-	while ( row >= 0 ) {
-		column = 1;
-		while ( column <= 6 ) {
-			if (row % 3 == 0) {
-				cout << "#";
-			}
-			else {
-				cout << ( column % 6 ? "-" : ">" ); // output
-			}
-			++column;
-		}
-		--row;
-		cout << endl;
-	}
+
+
 }
