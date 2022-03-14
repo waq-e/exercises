32bebeda2be3bf13675582091c9c8a0500c6e2a7
waq-e <whomomlikes@hotmail.com>
2022-03-14 06:40:20 -0500

    Exercise 4.26

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 // Exercise 4.22: Mystery3.cpp
 #include <iostream>
-using namespace std;
-int main() {
 
+using namespace std;
+
+int main() {
+	cout << "Enter size to make a hollow square of (1-20): ";
+	int size{};
+	cin >> size;
+
+	for (int row = 0; row < size; row++) {
+		for (int column = 0; column < size; column++) {
+			if (row == 0 || row == size-1) {
+				cout << "*";
+			} else {
+				if (column == 0 || column == size-1) {
+					cout << "*";
+				} else {
+					cout << " ";
+				}
+			}
+		}
+		cout << '\n';
+	}
 
 }
