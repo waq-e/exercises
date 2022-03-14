8fdaf6412d9d75f4fb041812b1b32045de3e2790
waq-e <whomomlikes@hotmail.com>
2022-03-14 06:16:01 -0500

    Exercise 4.19

--- main.cpp
+++ main.cpp
@@ -5,16 +5,22 @@ using namespace std;
 
 int main() {
 	int counter{1};
+	int larger{0};
 	int largest{0};
 
 	while (counter <= 10) {
 		cout << "Enter #" << counter++ << ": ";
 		int number{};
 		cin >> number;
-		if (number > largest) {
+		if (number > larger && number > largest) {
+			larger = largest;
 			largest = number;
 		}
+		if (number > larger && number < largest) {
+			larger = number;
+		}
 	}
+	cout << "The larger number was " << larger << '\n';
 	cout << "The largest number was " << largest << '\n';
 
 	return 0;
